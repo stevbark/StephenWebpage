576c3eeb781f582bffff542fc856d184410f9f11
Stephen <sbarkleyyeung@gmail.com>
2018-05-05 19:33:13 -0400

    adding queue messages to front end html

--- pages/chatroom/chatroom.js
+++ pages/chatroom/chatroom.js
@@ -12,19 +12,18 @@ app.controller("chatroomCtrl", function ($scope) {
     sqs.config.update({
         accessKeyId: "AKIAINRUPUX6GPNIWYVQ",
         secretAccessKey: "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",
-        "region": "us-east-1" 
+        "region": "us-east-1"
     });
 
 
     var params = {
-        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/110014340829/ChatroomQueue',
+        QueueUrl: 'https://sqs.us-east-1.amazonaws.com/110014340829/chatroom.fifo',
 
 
-        MaxNumberOfMessages:2,
+        MaxNumberOfMessages: 10,
         MessageAttributeNames: [
             'STRING_VALUE',
         ],
-        ReceiveRequestAttemptId: 'STRING_VALUE',
         VisibilityTimeout: 0,
         WaitTimeSeconds: 0
 
@@ -33,7 +32,18 @@ app.controller("chatroomCtrl", function ($scope) {
     /* region:'sqs.us-east-1.amazonaws.com'*/
     sqs.receiveMessage(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data); // successful response
+        else {
+            console.log(data);
+            data.Messages.forEach(function(message) {
+                console.log(message.Body);
+                addToBody(message.Body); // successful response
+            });
+
+        }
     });
+
+    function addToBody(textBody) {
+        document.querySelector("#chatBody").innerHTML += "<div> " + textBody + " </div>"
+    }
     //   SendMessage("https://sqs.us-east-1.amazonaws.com/110014340829/ChatroomQueue");*/
 });
