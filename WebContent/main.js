ad05e7ff11f81e29557c0e8b3f3da85961bb974c
stephen <sbarkleyyeung@gmail.com>
2020-12-21 12:52:00 -0800

    Changes
    Removed the chatroom while it is not ready. Also  updated the styling of the resume to be more inline with the PDF copy and added a link to my Linkden and added a new graph and a reset button for the old graph.

--- WebContent/main.js
+++ WebContent/main.js
@@ -31,24 +31,5 @@ app.config(function ($routeProvider) {
 
 });
 
-//https://stackoverflow.com/questions/12197880/angularjs-how-to-make-angular-load-script-inside-ng-include
-/*(function (ng) {
-  'use strict';
 
-  var app = ng.module('ngLoadScript', []);
 
-  app.directive('script', function() {
-    return {
-      restrict: 'E',
-      scope: false,
-      link: function(scope, elem, attr) {
-        if (attr.type === 'text/javascript-lazy') {
-          var code = elem.text();
-          var f = new Function(code);
-          f();
-        }
-      }
-    };
-  });
-
-}(angular));*/
--- WebContent/pages/survey/survey.js
+++ WebContent/pages/survey/survey.js
@@ -25,18 +25,25 @@ app.controller("surveyCtrl", function ($scope, $http) {
         });
    
      $scope.clickOption = function($number){
-        console.log("number " + $number + " selected");
-        var index = $number -1;
-        switch(index){
+        
+          
+        switch($number){
           case 0:
+            o1 = 1;
+            o2 = 1;
+            chart.options.data[0].dataPoints[0]=({y:o1,label: "Option 1"});
+            chart.options.data[0].dataPoints[1]=({y:o2,label: "Option 2"});
+            break;
+          case 1:
             o1 = o1+1;
             chart.options.data[0].dataPoints[0]=({y:o1,label: "Option 1"});
             break;
-          case 1:
+          case 2:
             o2 = o2+1;
             chart.options.data[0].dataPoints[1]=({y:o2,label: "Option 2"});
             break;  
         }
+        
 
         chart.render();
         
@@ -44,7 +51,47 @@ app.controller("surveyCtrl", function ($scope, $http) {
      }
 
 
-
+var chart2 = new CanvasJS.Chart("chartContainer2", {
+  animationEnabled: true,
+  
+  title:{
+    text:"Fortune 500 Companies by Country"
+  },
+  axisX:{
+    interval: 1
+  },
+  axisY2:{
+    interlacedColor: "rgba(1,77,101,.2)",
+    gridColor: "rgba(1,77,101,.1)",
+    title: "Number of Companies"
+  },
+  data: [{
+    type: "bar",
+    name: "companies",
+    axisYType: "secondary",
+    color: "#014D65",
+    dataPoints: [
+      { y: 3, label: "Sweden" },
+      { y: 7, label: "Taiwan" },
+      { y: 5, label: "Russia" },
+      { y: 9, label: "Spain" },
+      { y: 7, label: "Brazil" },
+      { y: 7, label: "India" },
+      { y: 9, label: "Italy" },
+      { y: 8, label: "Australia" },
+      { y: 11, label: "Canada" },
+      { y: 15, label: "South Korea" },
+      { y: 12, label: "Netherlands" },
+      { y: 15, label: "Switzerland" },
+      { y: 25, label: "Britain" },
+      { y: 28, label: "Germany" },
+      { y: 29, label: "France" },
+      { y: 52, label: "Japan" },
+      { y: 103, label: "China" },
+      { y: 134, label: "US" }
+    ]
+  }]
+});
 
 
 
@@ -55,6 +102,7 @@ app.controller("surveyCtrl", function ($scope, $http) {
 
        
         chart.render();
+        chart2.render();
     }
     $scope.init();
    
